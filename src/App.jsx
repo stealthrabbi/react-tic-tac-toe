317cab255f421c30cd48ac7d61a24d0318cdda6c
Mark Freedman <mark.freedman@gmail.com>
2024-02-03 10:22:50 -0500

    Active player as a derived state

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Log from './components/Log.jsx'
 import GameBoard from './components/GameBoard.jsx'
 import { useState } from 'react'
 
-function App() {
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 
-  const [activePlayer, setActivePlayer] = useState('X');
+function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X")
     setGameTurns(prevTurns => {
       // set current player to be opposite of most recent turn
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       // copy the previous turns, and add the new turn
       const updatedTurns = [{ square: { rowIndex, colIndex }, player: currentPlayer }, ...prevTurns];
       return updatedTurns;
